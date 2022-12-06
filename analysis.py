2d60aeb875fcf9d553b2fe621662edb7e8e6ba5a
cgtyagi <102990003+cgtyagi@users.noreply.github.com>
2022-12-05 19:34:31 -0800

    test2

--- analysis.py
+++ analysis.py
@@ -1,1 +1,16 @@
-#test
+#Setup
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing
+import shlex 
+sns.set()
+sns.set_context("paper", font_scale=10)
+
+#Data Cleaning
+data = pd.read_csv('SampleTaskData[42][77][59].csv')
+data.head()
+
+
+
